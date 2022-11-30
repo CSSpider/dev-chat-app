27c769002609e3fe55eae19ddf9fcca214676238
Daria Mordvinov <daria.mordvinov@gmail.com>
2022-11-30 09:20:33 -0500

    conditional rendering of sign up or sign in form

--- client/components/auth.jsx
+++ client/components/auth.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import { useForm } from 'react-hook-form';
 import loginPic from '../static/login.png';
@@ -14,24 +14,47 @@ const mapDispatchToProps = dispatch => {
 
 const Auth = (props) => {
     const {register, handleSubmit, formState: { errors } } = useForm();
+    const [needsSignUp, setSignUp] = useState(false);
     
 
     const onSubmit = data =>  {
         props.signInUser(data);
     }
 
+    const handleSetSignUp = () => {
+        console.log('inside a handlesignup')
+        const newNeedsSignUp = needsSignUp ? false : true;
+        setSignUp(newNeedsSignUp);
+    }
+
+    const textForButton = needsSignUp ? "Log in" : "Sign up";
+
     return (
         <main className="auth">
-            <div>
-                <h1 className="title-auth-form">Log in </h1>
-                <img src={loginPic} />
+            {!needsSignUp &&
+            <>
+                <div>
+                    <h1 className="title-auth-form">Log in </h1>
+                    <img src={loginPic} />
+                </div>
+                <form onSubmit={handleSubmit(onSubmit)} className="login-form">
+                    <input className="auth-input" {...register("username", { required: true })} name="username" type="text" />
+                    <input className="auth-input" {...register("email", { required: true })} name="email" type="email" />
+                    <input className="auth-input" {...register("password", { required: true })} name="password" type="password" />
+                    <input className="auth-input" type="submit" />
+                </form>
+            </>
+            } 
+            {needsSignUp && console.log('here is going to be sign up')
+                // place for Sign Up form
+            }
+
+            <div className="switch-form">
+                {!needsSignUp && <h1>Don't have an account?</h1>}
+                {needsSignUp && <h1>Already have an account?</h1>}
+                <button onClick={() => handleSetSignUp()}>{textForButton}</button>
             </div>
-            <form onSubmit={handleSubmit(onSubmit)} className="login-form">
-                <input className="auth-input" {...register("username", { required: true })} name="username" type="text" />
-                <input className="auth-input" {...register("email", { required: true })} name="email" type="email" />
-                <input className="auth-input" {...register("password", { required: true })} name="password" type="password" />
-                <input className="auth-input" type="submit" />
-            </form>
+
         </main>
     );
 }
