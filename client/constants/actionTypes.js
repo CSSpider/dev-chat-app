fa536115735207b48fed2370bf13e2ee5efc09cc
Willem Rosenthal <willemrosenthal@gmail.com>
2022-11-30 17:15:45 -0500

    codebox renders for everyone

--- client/constants/actionTypes.js
+++ client/constants/actionTypes.js
@@ -4,3 +4,6 @@ export const LOAD_USERS = 'LOAD_USERS'
 // auth action types
 export const LOGIN_USER = 'LOGIN_USER'
 export const SIGN_UP_USER = 'SIGN_UP_USER'
+
+// action for code edditor
+export const UPDATE_CODEVIEW = 'UPDATE_CODEVIEW'
