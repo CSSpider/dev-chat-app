1552f9bdcf4c829871f9e0af16a68c4026450486
Jackson Kalmbach <jacksonrkalmbach@gmail.com>
2022-11-30 11:03:34 -0500

    merge conflicts resolved

--- client/App.js
+++ client/App.js
@@ -2,39 +2,31 @@ import React, { useEffect } from 'react';
 import ChatContainer from './containers/chat-container';
 import  MessageContainer from './containers/message-container';
 import { FriendsContainer } from './containers/friends-list-container';
-<<<<<<< HEAD
 import Navbar from './containers/Navbar';
 import Display from './containers/Display';
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-=======
 import { useSelector, useDispatch } from "react-redux";
 import Auth from './components/auth';
->>>>>>> dev
+import "./styles.css";
 
 const App = () => {
 
   const username = useSelector(state => state.users.currentUser);
      
   return (
-<<<<<<< HEAD
-    <div>
-      <Navbar />
-      <Display />
-      {/* <FriendsContainer />
-      <ChatContainer /> */}
-    </div>
-=======
     <>
       {username &&
         <div>
-          {username}
-          <h1>App!</h1>
-          <FriendsContainer/>
-          <ChatContainer/>
+          
+          {/* <FriendsContainer/> */}
+          {/* <ChatContainer/> */}
+          <Navbar />
+          {/* {username} */}
+          <Display />
+          
         </div>}
       {!username && <div className="auth-container"><Auth /></div>}
     </>
->>>>>>> dev
   );
 }
 
