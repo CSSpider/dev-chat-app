71654a6c684e945ee09e0232af0345bd6365f1c7
Jo Huang <johuang@Jos-MacBook-Air.local>
2022-11-30 15:36:44 -0500

    added messages table

--- server/models/tables.js
+++ server/models/tables.js
@@ -12,24 +12,41 @@ const db = require('./database.js');
 	// 	);
 // 	`
 
+const messagesTable = `
+CREATE TABLE messages (
+	_id int NOT NULL GENERATED ALWAYS AS IDENTITY,
+	sender varchar NOT NULL,
+	receiver varchar(255) NOT NULL,
+	body varchar(255) NOT NULL,
+	date TIMESTAMP NOT NULL DEFAULT current_timestamp,
+	PRIMARY KEY (_id)
+	);
+`
+// const testInsertUserQuery = `
+// 	INSERT INTO users (firstname, lastname, username, password, email)
+//  	VALUES ('im', 'admin', 'admin', 'admin', 'admin@gmail.com');
+// 	`
+
 const testInsertUserQuery = `
-	INSERT INTO users (firstname, lastname, username, password, email)
- 	VALUES ('im', 'admin', 'admin', 'admin', 'admin@gmail.com');
+	INSERT INTO messages (sender, receiver, body)
+	VALUES ('hello', 'hello', 'it is me');
 	`
+
 const queryDynamicInsert = `
 	INSERT INTO users (firstname, lastname, username, password, email)
 	VALUES ($1, $2, $3, $4, $5);`
-// db.query(testInsertUserQuery)
-// 	.then(data => console.log(data))
-// 	.catch(err => console.log(err))
+
+db.query(testInsertUserQuery)
+	.then(data => console.log(data))
+	.catch(err => console.log(err))
 
 // db.query('SELECT * FROM users')
 // 	.then((res) => console.log('user:', res.rows[0]))
 
-const selectPasswordQuery= `
-	SELECT password FROM users WHERE username='admin';
-	`
+// const selectPasswordQuery= `
+// 	SELECT password FROM users WHERE username='admin';
+// 	`
 
-db.query(selectPasswordQuery)
-	.then(res => console.log(res.rows[0]))
+// db.query(selectPasswordQuery)
+// 	.then(res => console.log(res.rows[0]))
 
