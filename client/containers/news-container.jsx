1e2eb9b3557016d7f29f9b6175d91cbe7fb829a4
Daria Mordvinov <daria.mordvinov@gmail.com>
2022-12-01 16:22:24 -0500

    news are consoled

--- client/containers/news-container.jsx
+++ client/containers/news-container.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import * as actions from "../actions/action-creators";
 
@@ -6,15 +6,32 @@ const News = () => {
     //get news data from state
     // const newsData = useSelector(state => state.news);
     const dispatch = useDispatch();
-    const news = useSelector(state => state.news.news);
 
     useEffect( () => {
         console.log("in use effect");
-        dispatch(actions.getNewsActionCreator());
+        const func = async() => {
+            await dispatch(actions.getNewsActionCreator());
+        }
+        func();
     }, []);
+
+
+    const news = useSelector(state => state.news.news);
+    console.log(news);
+
+    const arr = [];
+    news.forEach(article => {
+        console.log(article);
+        arr.push(
+            <div>
+                <h4>{article.title}</h4>
+                <a href={article.url}>Link to the article</a>
+            </div>
+        )});
 
     return (
         <div className="news-container">
+            {arr}
         </div>
     )
 }
